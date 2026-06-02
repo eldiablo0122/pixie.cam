b8cb1f557e5b37124cc0d79465e080b3c58a3427
eldiablo0122 <nicolephileas@gmail.com>
2026-06-03 05:07:21 +0800

    Add Firebase configuration file
    This file contains the Firebase configuration settings for the application.

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Copy this file to firebase-config.js and replace the placeholder values with your real Firebase app values.
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyC3yURmqHxOy9ZBKPJ-h4ADY0PzGDz1T3Q",
+  authDomain: "pixiecam-e4bc7.firebaseapp.com",
+  projectId: "pixiecam-e4bc7",
+  storageBucket: "pixiecam-e4bc7.firebasestorage.app",
+  messagingSenderId: "68107632780",
+  appId: "1:68107632780:web:44db77b9be0b366e26bba9",
+};
